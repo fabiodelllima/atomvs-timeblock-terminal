0663a04114270a483115251c6133486052e7a769
fabiodelllima <f.f.fabiodelima@gmail.com>
2025-10-16 05:18:36 -0300

    feat(models): Exporta TimeLog unificado

--- cli/src/timeblock/models/__init__.py
+++ cli/src/timeblock/models/__init__.py
@@ -1,9 +1,10 @@
 """Data models for TimeBlock application."""
-from .event import ChangeLog, ChangeType, Event, EventStatus, PauseLog, TimeLog
+from .event import ChangeLog, ChangeType, Event, EventStatus, PauseLog
 from .habit import Habit, Recurrence
 from .habit_instance import HabitInstance
 from .routine import Routine
 from .task import Task
+from .time_log import TimeLog
 
 __all__ = [
     "Event",
