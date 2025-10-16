754e3b75981b903e59df0513c1e697c2dc34ff51
fabiodelllima <f.f.fabiodelima@gmail.com>
2025-10-16 07:21:41 -0300

    refactor(database): Exporta get_engine e get_engine_context

--- cli/src/timeblock/database/__init__.py
+++ cli/src/timeblock/database/__init__.py
@@ -1,4 +1,4 @@
 """Database utilities."""
-from .engine import create_db_and_tables
+from .engine import create_db_and_tables, get_engine, get_engine_context
 
-__all__ = ["create_db_and_tables"]
+__all__ = ["create_db_and_tables", "get_engine", "get_engine_context"]
